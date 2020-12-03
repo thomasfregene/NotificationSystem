c99090c66da58a88217eda48304553d172449d97
thomasfregene <fregzythomas@gmail.com>
2020-12-04 00:38:35 +0100

    created NotificationEmailAlert controller and index view

---
+++ Notification.Api/Controllers/NotificationEmailAlertController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Notification.Api.Controllers
+{
+    public class NotificationEmailAlertController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
